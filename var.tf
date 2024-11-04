351686b99313dd567764c395415d40f7550a187a
aashil2495 <120294200+aashil2495@users.noreply.github.com>
2024-11-03 22:09:30 -0600

    Add files via upload

---
+++ var.tf
@@ -0,0 +1,36 @@
+# Define variables for reusability
+variable "resource_group_name" {
+  default = "terraform_Databricks_Fitbit"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "databricks_workspace_name" {
+  default = "terraform_Fitbit_dev"
+}
+
+variable "adls_account_name" {
+  default = "terraformfitbitadlsg2dev"
+}
+
+variable "databricks_access_connector_name" {
+  default = "terraform_fitbit_databricks-access-connector"
+}
+
+variable "metastore_name" {
+  default = "metastore_azure_eastus"
+}
+
+variable "metastore_container_name" {
+  default = "metastore-root"
+}
+
+variable "managed_container_name"{
+    default="managed"
+}
+
+variable "unmanaged_container_name"{
+    default="unmanaged"
+}
